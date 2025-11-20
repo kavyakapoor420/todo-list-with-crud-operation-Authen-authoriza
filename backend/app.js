df2442c79fa4773e510e6fc328732aaa3ccd3927
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-11-20 22:49:40 +0530

    created auht and notes routes

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,10 @@ const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
 
+const protected=require('./Middlewares/authMiddleware')
+const { getNotes, createNotes, updateNotes, deleteNote } = require('./Controllers/noteController')
+const { register, login, getMe } = require('./Controllers/authController')
+
 
 const app=express() 
 
@@ -14,15 +18,24 @@ const MONGO_URI=process.env.MONGO_URI
 
 mongoose.connect(MONGO_URI)
         .then(()=>{
-            console.log("mongodb connected")
+            console.log("mongodb connected")     
+        }).catch((err)=>{
             console.log(err)
         })
 
+app.post('/api/auth/register',register)
+app.post('/api/auth/login',login)
+app.get('/api/auth/me',protected,getMe)
 
-app.get('/api/notes',)
+
+app.get('/api/notes',protected,getNotes)
+app.post('/api/notes',protected,createNotes)
+app.put('/api/notes/:id',protected,updateNotes)
+app.delete('/api/notes/:id',protected,deleteNote)
 
 
-app.listen(3000,()=>[
+
+app.listen(3000,()=>{
 
     console.log('app is listening on port 3000')
-])
+})
