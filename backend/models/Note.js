0818ea9453a23e187a55c54bda09fc9d0bd6ede6
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-11-20 22:11:00 +0530

    created schema for notes and user

---
+++ backend/models/Note.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose')
+
+const NoteSchema=new mongoose.Schema({
+
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true 
+    },
+    title:{
+        type:String , required:true 
+    },
+    content:{
+        type:String,required:true 
+    },
+    tags:{
+        type:[String],default:[]
+    },
+    color:{
+        type:String, default:"#fff"
+    }
+},{timestamps:true})
+
+
+module.exports=mongoose.model("Note",NoteSchema)
+
